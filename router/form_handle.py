e0d76b834a516dfc8aa74f6f19aa727d34e0d94c
Daniel <katzz.daniel@gmail.com>
2024-05-29 00:03:57 +0300

    deleted temp commented function

--- router/form_handle.py
+++ router/form_handle.py
@@ -26,25 +26,4 @@ async def submit(data: FormData):
     append_data(data.__root__)
     return RedirectResponse(url=f"/page{int(page_name[4:])+1}", status_code=303)
 
-# @router.post("/submit_form", response_class=HTMLResponse)
-# async def submit_form(request: Request, **kwargs):
-#     # Create a dictionary with the form data
-#     form_data = {
-#         'form_data': kwargs
-#     }
-#
-#     # Append form data to the list
-#     form_data_list.append(form_data)
-#
-#     # Write updated data back to JSON file
-#     with open('data/data.json', 'w') as file:
-#         json.dump(form_data_list, file, indent=4)
-#
-#     # Prepare context to pass to template
-#     context = {
-#         "request": request,
-#         "message": "Form data submitted successfully!",
-#     }
-#
-#     # Render the template with updated context (adjust as per your needs)
-#     return templates.TemplateResponse("page1.html", context)
+

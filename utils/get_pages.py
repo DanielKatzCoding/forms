84e4fbf334dc958e7920410a2ad7fce4be625f49
Daniel Katz <katzz.daniel@gmail.com>
2024-05-31 16:27:17 +0300

    buttons now show the its page title

--- utils/get_pages.py
+++ utils/get_pages.py
@@ -14,17 +14,17 @@ def add_routes(directory, prefix):
     templates_path = Path(directory)
     lst_pages = [page for page in templates_path.glob("*.html")]
     dict_pages = {"pages": [f"{prefix}/{page.name.rsplit('.')[0]}" for page in lst_pages]}
-    print(dict_pages)
+
     for template_file in lst_pages:
         route_path = f"{prefix}/{template_file.stem}"
         print(route_path)
         tmp_data = dict_pages.copy()
 
         with open("data/data.json", 'r') as f:
-            tmp_data.update(json.load(f))
+            tmp_data.update(json.load(f)[prefix.lstrip('/')])
 
         async def route_func(request: Request):
-            return templates.TemplateResponse(route_path + ".html", {"request": request, "data": tmp_data})
+            return templates.TemplateResponse(route_path + ".html", {"request": request, "data": tmp_data,"zip": zip})
 
         router.add_api_route(route_path, route_func, methods=["GET"])
 

12950136c6a8ff259dcf0c3dddafa3c160be3520
Daniel <katzz.daniel@gmail.com>
2024-06-05 00:02:49 +0300

    loading data to pages is now available!

--- utils/get_pages.py
+++ utils/get_pages.py
@@ -4,31 +4,46 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+
 router = APIRouter()
 
 # Initialize templates to point to the root of your templates directory
 templates = Jinja2Templates(directory="templates")
 
 
+def load_data(path: str) -> dict | None:
+    with open("data/data.json", 'r') as f:
+        form_data = json.load(f)
+        try:
+            form_data = form_data[path.split('/')[1]][path]
+        except:
+            return
+        return form_data
+
+
 def add_routes(directory, prefix):
     templates_path = Path(directory)
-    lst_pages = [page for page in templates_path.glob("*.html")]
+    # lst_pages = [page for page in templates_path.glob("*.html")]
+    # dict_pages = {"pages": [f"{prefix}/{page.name.rsplit('.')[0]}" for page in lst_pages]}
+    #
+    lst_pages = sorted([t_path for t_path in templates_path.glob("*.html")])
     dict_pages = {"pages": [f"{prefix}/{page.name.rsplit('.')[0]}" for page in lst_pages]}
 
     for template_file in lst_pages:
         route_path = f"{prefix}/{template_file.stem}"
-        print(route_path)
         tmp_data = dict_pages.copy()
 
         with open("data/data.json", 'r') as f:
             tmp_data.update(json.load(f)[prefix.lstrip('/')])
 
-        async def route_func(request: Request):
-            return templates.TemplateResponse(route_path + ".html", {
+        async def route_func(request: Request, template_name=route_path):
+            a = [template_name + ".html", {
                 "request": request,
                 "data": tmp_data,
+                "form_data": load_data(template_name),
                 "zip": zip
-            })
+            }]
+            return templates.TemplateResponse(*a)
 
         router.add_api_route(route_path, route_func, methods=["GET"])
 
